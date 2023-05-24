139d9e37b0c4a06e1254387fa48d6ccf7aa98a94
Luis Delgado <>
2023-05-23 20:48:41 -0400

    Order tasks by colums and allow user change states for tasks

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -10,13 +10,14 @@ import { useEffect, useState } from 'react';
 
 function TaskCard(props) {
 
-    const { taskInfo } = props;
+    const { taskInfo, onPressButtonTask } = props;
 
     return (
         <div style={{ background: 'grey', margin: '15px' }}>
             <p>Name: {taskInfo?.name}</p>
             <p>Id: {taskInfo?.id}</p>
             <p>State: {taskInfo?.state}</p>
+            <button onClick={() => onPressButtonTask(taskInfo)}>NEXT</button>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,12 @@ function fetchTasks() {
   return [
     {
       id: 1,
-      state: 'NEW',
+      state: 'ON_PROGRESS',
       name: 'Maketar frontend'
     },
     {
       id: 2,
-      state: 'NEW',
+      state: 'ON_PROGRESS',
       name: 'Implementar endpoints de backend'
     },
     {
@@ -46,28 +46,89 @@ function Home() {
 
   useEffect(() => {
     console.log('on mount')
-    setTimeout(() => {
-      const tasks = fetchTasks() // "servicio de backend que me retorna tareas"
-      setTasksState(tasks)
-      setLoading(false)
-    }, 5000)
+    // NOTE: Simulate fetch data from some backend.
+    // setTimeout(() => {
+    //   const tasks = fetchTasks() // "servicio de backend que me retorna tareas"
+    //   setTasksState(tasks)
+    // }, 5000)
+    const tasks = fetchTasks() // "servicio de backend que me retorna tareas"
+    setTasksState(tasks)
+    setLoading(false)
   }, [])
 
-  useEffect(() => {
-    console.log('componente se esta actualizando')
-  }, [tasksState])
+  // useEffect(() => {
+  //   console.log('componente se esta actualizando')
+  // }, [tasksState])
+
+  const filterDataByState = (state) => {
+    const filterTasks = tasksState.filter((value) => {
+      return value.state === state
+    });
+    return filterTasks;
+  }
+
+  const setNewStateForTask = (newState, task) => {
+    const filterOtherTasks = tasksState.filter((value) => {
+      return value.id !== task.id
+    })
+    task.state = newState;
+    filterOtherTasks.push(task);
+    setTasksState(filterOtherTasks);
+  }
+
+  const onPressButtonTask = (task) => {
+    switch (task.state) {
+      case 'TODO':
+        setNewStateForTask('ON_PROGRESS', task)
+        break;
+
+      case 'ON_PROGRESS':
+        setNewStateForTask('FINISH', task)
+        break;
+
+      case 'FINISH':
+        setNewStateForTask('DELIVERY', task)
+        break;
+    
+      default:
+        break;
+    }
+  }
 
   if (loading) {
     return 'Cargando...'
   }
 
   return (
-    <div className="Home">
-      {tasksState.map((value, index) => {
-        return (
-          <TaskCard taskInfo={value} key={index}/>
-        )
-      })}
+    <div className="Home" style={{ 
+      display: 'flex',
+      justifyContent: 'center',
+      textAlign: 'center'
+     }}>
+      <div>
+        TODO
+        {filterDataByState('TODO').map((value, index) => {
+          return (
+            <TaskCard taskInfo={value} key={index} onPressButtonTask={onPressButtonTask}/>
+          )
+        })}
+      </div>
+      <div>
+        ON_PROGRESS
+        {filterDataByState('ON_PROGRESS').map((value, index) => {
+          return (
+            <TaskCard taskInfo={value} key={index } onPressButtonTask={onPressButtonTask}/>
+          )
+        })}
+      </div>
+      <div>
+        FINISH
+        {filterDataByState('FINISH').map((value, index) => {
+          return (
+            <TaskCard taskInfo={value} key={index} onPressButtonTask={onPressButtonTask}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
